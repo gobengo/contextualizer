8af0ac1d8c9f7804f2d7b449a6ac5d4a964e79a1
Benjamin Goering <ben@bengo.co>
2020-12-01 18:02:30 -0800

    proof of concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from '@rjsf/material-ui';
+import { JSONSchema7 } from "json-schema";
+
+const iriReferenceSchema: JSONSchema7 = {
+  type: 'string',
+  title: "IRI Reference",
+}
+
+const iriSchema: JSONSchema7 = {
+  type: 'string',
+  title: 'Internationalized Resource Identifier (IRI)'
+}
+
+const compactIriSchema: JSONSchema7 = {
+  type: 'string',
+  title: 'Compact IRI',
+}
+
+/**
+ * via https://www.w3.org/TR/json-ld11/#context-definitions
+ * A context definition MUST be a map whose keys MUST be either terms, compact IRIs, IRIs, or one of the keywords @base, @import, @language, @propagate, @protected, @type, @version, or @vocab. 
+ */
+const contextDefinitionSchema: JSONSchema7 = {
+  type: "object",
+  title: "JSON-LD Context Definition",
+  // The value of keys that are not keywords MUST be either an IRI, a compact IRI, a term, a blank node identifier, a keyword, null, or an expanded term definition.
+  additionalProperties: {
+    title: "Context Definition non-keyword key/value",
+    oneOf: [
+      iriSchema,
+      compactIriSchema,
+      { type: 'string', title: 'term' },
+      { type: 'string', title: 'blank node identifier' },
+      { type: 'string', title: 'keyword', },
+      { type: 'null', title: 'null' },
+      // https://www.w3.org/TR/json-ld11/#dfn-expanded-term-definition
+      { type: 'object', title: 'expanded term definition' },
+    ]
+  },
+}
+
+const jsonldContextItemSchema: JSONSchema7 = {
+  title: "JSON-LD @context item",
+  oneOf: [
+    iriReferenceSchema,
+    contextDefinitionSchema,
+    { type: "null", title: 'Null', },
+  ]
+}
+
+/**
+ * via: https://www.w3.org/TR/json-ld11/#keywords
+ * "The value of @context MUST be null, an IRI reference, a context definition, or an array composed of any of these."
+ */
+const jsonldContextSchema: JSONSchema7 = {
+  oneOf: [
+    jsonldContextItemSchema,
+    {
+      title: `Array of ${jsonldContextItemSchema.title}`,
+      type: "array",
+      items: jsonldContextItemSchema,
+    }
+  ]
+}
+
+const log = (type: string) => console.log.bind(console, type);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Narrator
         </a>
       </header>
+      <section>
+        <Form schema={jsonldContextSchema}
+          onChange={log("changed")}
+          onSubmit={log("submitted")}
+          onError={log("errors")} />
+      </section>
     </div>
   );
 }
